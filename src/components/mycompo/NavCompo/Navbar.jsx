889af63013291b4fd2a13055f95ce79852fa8bc8
Sumit <itzsumitr995@gmail.com>
2026-01-22 19:51:29 +0530

    Reforms in dynamics

--- src/components/mycompo/NavCompo/Navbar.jsx
+++ src/components/mycompo/NavCompo/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     
     return (
         <div className='w-full h-[10vh] flex items-center justify-center  relative z-20'>
-            <div className='flex w-1/2 justify-between items-center fixed left-[27vw]  m-0.75  bg-transparent  '>
+            <div className=' w-[52%] flex items-center justify-between fixed top-0 dark:bg-black bg-white '>
                 <NavImage/>
                 <Navlists/>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,29 +3,30 @@ import HomeProfile from '@/components/mycompo/HomeCompo/HomeProfile'
 import HomeLinks from "../components/mycompo/HomeCompo/HomeLinks";
 import HomeButtons from '@/components/mycompo/HomeCompo/HomeButtons'
 import React from 'react'
-import { Button } from '@/components/ui/button';
-import { Accordion } from '@/components/ui/accordion';
 import WorkExperience from '@/components/WorkExperience/WorkExperience';
+
 
 const Home = () => {
 
 
   return (
     <div className='flex flex-col '>
-      <div className=' ml-[27vw] w-1/2 h-[90vh]' >
-        <HomeProfile />
-        <HomeHeading />
-        <HomeButtons />
-        <HomeLinks />
+      <div className=' w-full h-20vh flex items-center justify-center flex-col' >
+        <div className='w-1/2'>
+          <HomeProfile />
+          <HomeHeading />
+          <HomeButtons />
+          <HomeLinks />
 
-      </div>
-      <div className=' w-1/2 ml-[27vw]  '>
-      <div className='text-sm font-semibold dark:text-zinc-700 text-zinc-300'>Featured</div>
-      <div className='text-zinc-500 dark:text-zinc-300 font-bold text-2xl '>Experience</div>
-       <WorkExperience/>
-      <div className='text-sm font-semibold dark:text-zinc-700 text-zinc-300 mt-5 '>Featured</div>
-      <div className='text-zinc-500 dark:text-zinc-300 font-bold text-2xl'>Projects</div>
-
+        </div>
+        <div className=' w-1/2 '>
+          <div className='text-sm font-semibold dark:text-zinc-700 text-zinc-300'>Featured</div>
+          <div className='text-zinc-500 dark:text-zinc-300 font-bold text-2xl '>Experience</div>
+          <WorkExperience />
+          <div className='text-sm font-semibold dark:text-zinc-700 text-zinc-300 mt-5 '>Featured</div>
+          <div className='text-zinc-500 dark:text-zinc-300 font-bold text-2xl'>Projects</div>
+          
+        </div>
       </div>
     </div>
 
