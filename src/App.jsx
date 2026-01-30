e11416e77fba6825514a4e80786dd531d305a6f7
Sumit <itzsumitr995@gmail.com>
2026-01-31 02:32:52 +0530

    added:AppRoutes.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,17 @@ import Certificate from "./pages/Certificate";
 import Contact from "./pages/Contact";
 import Oneko from "@/components/mycompo/Oneko";
 import { BlurFade } from "@/components/ui/blur-fade";
+import AppRoutes from "./AppRoutes";
 
 export default function App() {
   return (
     <>
       <Navbar />
-
       {/* Viewport pet */}
       <Oneko />
       <o-neko className="fixed z-99" speed="10"></o-neko>
-
       <BlurFade>
-        <div className="w-screen pt-[10vh]">
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/projects' element={<Project />} />
-            <Route path='/certificates' element={<Certificate />} />
-            <Route path='/contact' element={<Contact />} />
-          </Routes>
-        </div>
+        <AppRoutes />
       </BlurFade>
     </>
   );
--- src/components/mycompo/HomeCompo/HomeProfile.jsx
+++ src/components/mycompo/HomeCompo/HomeProfile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomeProfile = () => {
     return (
-        <div className='h-[25vh] w-full relative '>
+        <div className='h-[28vh] w-full relative '>
             <div className="w-25 h-25 rounded-[60px] bg-amber-100
              bg-[url('/Profile-pic.png')] border-2 border-black
              bg-cover bg-center bg-no-repeat
